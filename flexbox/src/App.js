757770078ee7e10c0a3a828a50f450ea415f91ce
unknown <victoroluwabori2@gmail.com>
2024-04-06 12:45:59 -0700

    grid-template is changed to 4 by 4 from 2 by 2

---
+++ flexbox/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my first React class and I really Love it</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ flexbox/src/Flexbox.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Flexbox.css"
+function Flexbox() {
+    return (
+        <div>
+            <div className="container_flex">
+                <h2 className="link1">Home</h2>
+                <h2 className="link1">News Update</h2>
+                <h2 className="link1">Sport</h2>
+                <h2 className="link1">Entertainment</h2>
+                <h2 className="link1">Politics</h2>
+                <h2 className="link1">Religion</h2>
+                <h2 className="link1">Education</h2>
+            </div> <br />
+            <div className="grid_container">
+                <h2 className="grid1">Lifestyle</h2>
+                <h2 className="grid2">Fashion</h2>
+                <h2 className="grid3">Music</h2>
+                <h2 className="grid4">Information</h2>
+                
+                <h2 className="grid1">Lifestyle</h2>
+                <h2 className="grid2">Fashion</h2>
+                <h2 className="grid3">Music</h2>
+                <h2 className="grid4">Information</h2>
+
+                <h2 className="grid1">Lifestyle</h2>
+                <h2 className="grid2">Fashion</h2>
+                <h2 className="grid3">Music</h2>
+                <h2 className="grid4">Information</h2>
+
+                <h2 className="grid1">Lifestyle</h2>
+                <h2 className="grid2">Fashion</h2>
+                <h2 className="grid3">Music</h2>
+                <h2 className="grid4">Information</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Flexbox;
